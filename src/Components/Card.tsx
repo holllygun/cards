2e50fb5c1eab861c387754b115b7793240eda161
N.pushkar <pushkar.anastasiia@gmail.com>
2025-01-19 18:03:07 +0300

    edited

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -8,8 +8,7 @@ interface CardProps {
 
 export const Card: React.FC<CardProps> = ({image, title, text}) => {
   return (
-    <div className="card-columns">
-      <div className="card" style={{width: '18rem'}}>
+    <div className="card" style={{width: '18rem'}}>
       {image? <img src={image} className="card-img-top" alt="..."/>:null}
       <div className="card-body">
           <h5 className="card-title">{title}</h5>
@@ -17,6 +16,5 @@ export const Card: React.FC<CardProps> = ({image, title, text}) => {
           <a href="#" className="btn btn-primary">Go somewhere</a>
       </div>
     </div>
-  </div>
   )
 }
